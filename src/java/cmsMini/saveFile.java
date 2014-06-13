59c5619f7ddd06103634d52c06ebb28ad51ee020
muhammadwannous <muhammadwannous@192.168.11.57>
2014-06-13 18:21:28 +0900

    Remove ignored files

--- src/java/cmsMini/saveFile.java
+++ src/java/cmsMini/saveFile.java
@@ -73,7 +73,6 @@ public class saveFile extends HttpServlet {
             body.setParents(Arrays.asList(new ParentReference().setId(homeFolderId)));
             InputStreamContent contents = new InputStreamContent(null, file.getInputStream());
             contents.setLength(file.getSize());
-            contents.setCloseInputStream(true);
             Drive.Files.Insert insertRequest = serviceDrive.files().insert(body, contents);
             insertRequest.execute();
           }
--- web/home.jsp
+++ web/home.jsp
@@ -304,7 +304,6 @@
                               break;
                             }
                           }
-                          displayB = true;
                         }
                         if (displayB) {
                 %>
